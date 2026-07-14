c0975c2f1a8eacefaf8a882fd899ee2d52869d7f
Nadila <nadilarizkyamelia5@gmail.com>
2026-07-14 08:51:10 +0800

    Initial Commit

---
+++ dropTo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dropTo
+//
+//  Created by Nadila Rizky Amelia on 14/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dropTo/dropToApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropToApp.swift
+//  dropTo
+//
+//  Created by Nadila Rizky Amelia on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct dropToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
