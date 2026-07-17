90b57bacbdac958b55357f8233da9baa05d342d8
azhanumm <azhanumm@gmail.com>
2026-07-17 09:57:45 +0800

    moving

--- dropTo/ContentView.swift
+++ dropTo/ContentView.swift
@@ -1,24 +1,57 @@
 //
 //  ContentView.swift
-//  dropTo
+//  CH2
 //
-//  Created by Nadila Rizky Amelia on 14/07/26.
+//  Created by Nadila Rizky Amelia on 24/04/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var albumStore: AlbumStore
+    @State var showAddAlbum = false
+    @State var newAlbumName = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            AlbumView()
+                .navigationTitle("dropTo")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        HStack {
+                            if !albumStore.undoStack.isEmpty {
+                                Button { albumStore.undo() } label: {
+                                    Image(systemName: "arrow.uturn.backward.circle")
+                                }
+                            }
+                            Button {
+                                newAlbumName = ""
+                                showAddAlbum = true
+                            } label: {
+                                Image(systemName: "plus.circle")
+                            }
+                        }
+                    }
+                }
+                .toolbarTitleDisplayMode(.inlineLarge)
+                .alert("New Album", isPresented: $showAddAlbum) {
+                    TextField("Album Name", text: $newAlbumName)
+                    Button("Create") {
+                        if !newAlbumName.trimmingCharacters(in: .whitespaces).isEmpty {
+                            albumStore.addAlbum(name: newAlbumName)
+                        }
+                    }
+                    Button("Cancel", role: .cancel) {}
+                } message: {
+                    Text("Enter a name for this album")
+                }
         }
-        .padding()
-    }
+        
+        }
 }
 
 #Preview {
     ContentView()
+        .environmentObject(AlbumStore())
+        .environmentObject(PhotoLibraryManager())
 }
